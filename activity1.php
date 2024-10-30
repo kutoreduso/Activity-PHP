479ef78e589367e0225daec9d2da9fccda78b6b0
kutoreduso <kurtcantiga16@gmail.com>
2024-10-30 20:56:11 +0800

    Product Listed Added To Homepage

--- activity1.php
+++ activity1.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Activity 1</title>
 </head>
 <body>
-    
+    <h1>Vendo Machine</h1>
+        <form method="Post">
+            <fieldset style="width:500px;">
+
+            <legend>Products</legend>
+
+            <label>
+                <input type="checkbox" name="products[]" value="Coke"> Coke - P15
+            </label><br>
+
+            <label>
+                <input type="checkbox" name="product[]" value="Sprite"> Sprite - P20
+            </label><br>
+            
+            <label>
+                <input type="checkbox" name="product[]" value="Royal"> Royal - P20
+            </label><br>
+
+            <label>
+                <input type="checkbox" name="product[]" value="Pepsi"> Pepsi - P15
+            </label><br>
+
+            <label>
+                <input type="checkbox" name="product[]" value="Mountain Dew"> Mountain Dew - P20
+            </label><br>
+</fieldset>
+        </form>
 </body>
 </html>
